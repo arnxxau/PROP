7b34bd61865b2abc7631e8d8f79a3c2ab9cdc3bd
Ivian34 <ivancobosnavarro@gmail.com>
2023-11-11 17:33:08 +0100

    he creat el grid boolean

--- src/main/java/edu/upc/prop/clusterxx/Grid.java
+++ src/main/java/edu/upc/prop/clusterxx/Grid.java
@@ -1,4 +1,20 @@
 package edu.upc.prop.clusterxx;
 
+import java.io.IOException;
+
 public class Grid {
+    private boolean[][] grid;//diu si hi pot haver o no un caracter.
+    // Mostrar el teclado
+
+    //pre: col es numero de columnes per cada fila, i sep la separacio desde l'inici en columnes de cada fila.
+    //post: es crea un Grid buit demanant pasant les dades per paràmetre.
+    public Grid(boolean[][] mat) { //crea un grid buit
+        grid = new boolean[mat.length][mat[0].length];
+        for(int i=0;i< mat.length;i++){
+            for(int j=0;j<mat[i].length;j++){
+                grid[i][j]=mat[i][j];
+            }
+        }
+    }
+
 }
