ce4f737710d219b2160f725eae2231d7ed51b7ac
ivan.cobos <ivan.cobos@estudiantat.upc.edu>
2023-11-12 13:41:43 +0100

    grid es un vector de pares de posiciones donde se pueden poner letras del alfabeto del teclado

--- src/main/java/edu/upc/prop/clusterxx/Grid.java
+++ src/main/java/edu/upc/prop/clusterxx/Grid.java
@@ -1,20 +1,34 @@
 package edu.upc.prop.clusterxx;
 
 import java.io.IOException;
+import java.lang.reflect.Array;
+import java.util.ArrayList;
 
 public class Grid {
-    private boolean[][] grid;//diu si hi pot haver o no un caracter.
-    // Mostrar el teclado
+    private ArrayList<Pair> pares = new ArrayList<Pair>();
 
-    //pre: col es numero de columnes per cada fila, i sep la separacio desde l'inici en columnes de cada fila.
-    //post: es crea un Grid buit demanant pasant les dades per paràmetre.
+    //post: Construiex l'arraylist de Posicions on l'usuari ha escollit que es poden posar lletres de l'alfabet.
     public Grid(boolean[][] mat) { //crea un grid buit
-        grid = new boolean[mat.length][mat[0].length];
         for(int i=0;i< mat.length;i++){
             for(int j=0;j<mat[i].length;j++){
-                grid[i][j]=mat[i][j];
+                if(mat[i][j]){
+                    Pair par = new Pair(i,j);
+                    pares.add(par);
+                }
             }
         }
+    }
+    public Grid(){}
+    public double distance(Pair p1,Pair p2){
+        return Math.sqrt(Math.pow(p1.x-p2.x,2)+Math.pow(p1.y-p2.y,2));
+    }
+
+    public ArrayList<Pair> getPares() {
+        return pares;
+    }
+
+    public void setPares(ArrayList<Pair> pares) {
+        this.pares = pares;
     }
 
 }
