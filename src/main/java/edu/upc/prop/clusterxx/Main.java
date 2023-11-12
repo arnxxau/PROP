87f9791ed1a9722d8531d6e6d497ba9c9c5ddc21
arnxxau <arnxxau@gmail.com>
2023-11-12 20:53:10 +0100

    dead

--- src/main/java/edu/upc/prop/clusterxx/Main.java
+++ src/main/java/edu/upc/prop/clusterxx/Main.java
@@ -2,13 +2,18 @@ package edu.upc.prop.clusterxx;
 
 import com.google.gson.Gson;
 
-public class Main {
-  public static void main(String[] args) {
-    System.out.println("Hello world!");
-    new Gson();
+import java.io.FileNotFoundException;
 
-    Main divisioner = new Main();
-    System.out.println("Dividing 10 by 2 is " + divisioner.division(10,2));
+public class Main {
+  public static void main(String[] args) throws FileNotFoundException {
+    System.out.println("Hello world!");
+    //new Gson();
+
+    //Main divisioner = new Main();
+    //System.out.println("Dividing 10 by 2 is " + divisioner.division(10,2));
+
+    Frequency freq = new Frequency("test", "/Users/arnxxau/FIB/subgrup-prop12.5/src/main/java/edu/upc/prop/clusterxx/test.txt", 0);
+    freq.printFrequencies();
   }
 
   public float division(int a, int b) throws ArithmeticException {
