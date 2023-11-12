076d0d4ddf12c9bfd37ed923b57d52eeff39663a
arnxxau <arnxxau@gmail.com>
2023-11-12 23:58:02 +0100

    freq class functioning state

--- src/main/java/edu/upc/prop/clusterxx/Frequency.java
+++ src/main/java/edu/upc/prop/clusterxx/Frequency.java
@@ -21,7 +21,9 @@ public class Frequency {
     private Instant lastModifiedTime;
     private File f;
 
-    HashMap<Character, HashMap<Character, Double>> freq;
+    private Integer frequencyWeight;
+
+    HashMap<Character, HashMap<Character, Integer>> freq;
 
     private boolean verifyCharacter(Character c) {
         return true;
@@ -31,19 +33,21 @@ public class Frequency {
         return true;
     }
 
-    private void insertNewFreq(Character first, Character second, Double frequency, int mode) {
+    private void insertNewFreq(Character first, Character second, Integer frequency, int mode) {
         if (freq.containsKey(first)) {
-            HashMap<Character, Double> innerHashMap = freq.get(first);
+            HashMap<Character, Integer> innerHashMap = freq.get(first);
 
             if (mode == REPLACE) {
                 innerHashMap.put(second, frequency);
             } else if (mode == ADD){
                 if (innerHashMap.containsKey(second)) {
                     innerHashMap.put(second, innerHashMap.get(second) + frequency);
+                } else {
+                    innerHashMap.put(second, frequency);
                 }
             }
         } else {
-            HashMap<Character, Double> innerHashMap = new HashMap<>();
+            HashMap<Character, Integer> innerHashMap = new HashMap<>();
             innerHashMap.put(second, frequency);
             freq.put(first, innerHashMap);
         }
@@ -58,7 +62,11 @@ public class Frequency {
         this.name = name;
         f = new File(filePath);
         freq = new HashMap<>();
-        extractRawFrequenciesFromFile();
+        frequencyWeight = 0;
+
+        if (fileMode == FREQ_MODE) extractRawFrequenciesFromFile();
+        else if (fileMode == TEXT_MODE) extractTextFrequenciesFromFile();
+
 
         creationDate = Instant.now();
     }
@@ -71,46 +79,50 @@ public class Frequency {
      */
     private void extractRawFrequenciesFromFile() throws FileNotFoundException {
         Scanner scanner = new Scanner(f);
+        frequencyWeight = Integer.parseInt(scanner.nextLine());
         while (scanner.hasNextLine()) {
             String str = scanner.nextLine();
             //System.out.println(str);
             String[] line = str.split(" ");
             Character first = line[0].charAt(0);
             Character second = line[1].charAt(0);
-            Double d_freq = Double.parseDouble(line[2]);
+            Integer d_freq = Integer.parseInt(line[2]);
 
             insertNewFreq(first, second, d_freq, REPLACE);
         }
     }
 
     private void processWord(String w) {
+        for (int i = 0; i < w.length() - 1; ++i) {
+            insertNewFreq(w.charAt(i), w.charAt(i + 1), 1, ADD);
+            insertNewFreq(w.charAt(i + 1), w.charAt(i), 1, ADD);
+        }
         return;
     }
 
     private void extractTextFrequenciesFromFile() throws FileNotFoundException {
-        /*
         Scanner scanner = new Scanner(f);
         while (scanner.hasNextLine()) {
-            String str = scanner.nex();
-            //System.out.println(str);
-            String[] line = str.split(" ");
-            Character first = line[0].charAt(0);
-            Character second = line[1].charAt(0);
-            Double d_freq = Double.parseDouble(line[2]);
+            String str = scanner.next();
 
-            insertNewFreq(first, second, d_freq);
-        }*/
+            String[] line = str.split(" ");
+
+            frequencyWeight += line.length;
+
+            for (int i = 0; i < line.length; ++i) processWord(line[i]);
+        }
     }
 
 
 
     public void printFrequencies() {
-        for (Map.Entry<Character, HashMap<Character, Double>> outerEntry : freq.entrySet()) {
+        System.out.println("Freq weight = " + frequencyWeight);
+        for (Map.Entry<Character, HashMap<Character, Integer>> outerEntry : freq.entrySet()) {
             char outerKey = outerEntry.getKey();
             System.out.print(outerKey + ": {");
 
-            HashMap<Character, Double> innerMap = outerEntry.getValue();
-            for (Map.Entry<Character, Double> innerEntry : innerMap.entrySet()) {
+            HashMap<Character, Integer> innerMap = outerEntry.getValue();
+            for (Map.Entry<Character, Integer> innerEntry : innerMap.entrySet()) {
                 char innerKey = innerEntry.getKey();
                 double innerValue = innerEntry.getValue();
                 System.out.print(innerKey + "=" + innerValue + ", ");
@@ -131,11 +143,11 @@ public class Frequency {
         return words;
     }
 
-    public Date getCreationDate() {
+    public Instant getCreationDate() {
         return creationDate;
     }
 
-    public Date getLastModifiedTime() {
+    public Instant getLastModifiedTime() {
         return lastModifiedTime;
     }
 
