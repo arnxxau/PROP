6303d0172e8fb6e4c3009b45b9ab0d41f4edda47
nilallue <nil.allue@estudiantat.upc.edu>
2023-11-13 14:29:17 +0100

    [add] atributs i inici de clase

--- src/main/java/edu/upc/prop/clusterxx/Keyboard.java
+++ src/main/java/edu/upc/prop/clusterxx/Keyboard.java
@@ -1,4 +1,40 @@
 package edu.upc.prop.clusterxx;
 
+import java.time.Instant;
+
 public class Keyboard {
+    private String nom;
+    private Instant dataCreacio;
+
+    private Instant ultimaModificacio;
+
+    private char[] distribucio;
+
+    private Alphabet Alph;
+
+    private Frequency Freq;
+
+    private Grid Grid;
+
+
+
+    public Keyboard(String nom, Alphabet alphabet, Frequency frequency, Grid grid){
+        //S'ha de comprobar que la freq. es d'alfabet?
+
+        this.nom=nom;
+
+        dataCreacio=Instant.now();
+
+        Alph=alphabet;
+
+        Freq = frequency;
+
+        Grid = grid;
+
+
+
+
+
+    }
+
 }
