302250179b44ab28089e6c11d356119af8498b85
arnxxau <arnxxau@gmail.com>
2023-11-16 23:40:33 +0100

    AKIRAISDEAD changes

---
+++ src/test/java/edu.upc.prop.clusterxx/FrequencyTest.java
@@ -0,0 +1,33 @@
+package edu.upc.prop.clusterxx;
+
+
+import static org.mockito.Mockito.*;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+public class FrequencyTest {
+
+    @Test
+    public void getNumberOfAppearances() {
+    }
+
+    @Test
+    public void getFrequency() {
+    }
+
+    @Test
+    public void printFrequencies() {
+    }
+
+    @Test
+    public void getName() {
+    }
+
+    @Test
+    public void getCreationDate() {
+    }
+
+    @Test
+    public void getLastModifiedTime() {
+    }
+}
