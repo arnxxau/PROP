9d34cfb057b35ec6a0477c17d9d250f53d000576
Ivian34 <ivancobosnavarro@gmail.com>
2023-11-17 09:46:35 +0100

    ANDREU afegit Grid i Pair i sets a kb crec

--- src/main/java/edu/upc/prop/clusterxx/Grid.java
+++ src/main/java/edu/upc/prop/clusterxx/Grid.java
@@ -1,4 +1,45 @@
 package edu.upc.prop.clusterxx;
 
+import java.io.IOException;
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.Comparator;
+
+
 public class Grid {
+    private ArrayList<Pair> grid = new ArrayList<Pair>();
+
+    //pre: mat not null
+    //post: Construiex l'arraylist de Posicions on l'usuari ha escollit que es poden posar lletres de l'alfabet.
+    public Grid(boolean[][] mat) { //crea un grid buit
+        for(int i=0;i< mat.length;i++){
+            for(int j=0;j<mat[i].length;j++){
+                if(mat[i][j]){
+                    Pair par = new Pair(i,j);
+                    grid.add(par);
+                }
+            }
+        }
+    }
+    //pre: p not null
+    //post: construeix l'objecte Grid copiant p en grid.
+    public Grid(ArrayList<Pair> p){
+        for (Pair pair : p) grid.add(pair);
+    }
+
+    public Grid(){}
+
+    public double distance(int i1,int i2)throws IndexOutOfBoundsException{
+        return Math.sqrt(Math.pow(grid.get(i1).x-grid.get(i2).x,2)+Math.pow(grid.get(i1).y-grid.get(i2).y,2));
+    }
+
+    public ArrayList<Pair> getGrid() {
+        return grid;
+    }
+
+    //el puntero cambia
+    public void setGrid(ArrayList<Pair> pares) {
+        this.grid = pares;
+    }
+
 }
--- src/main/java/edu/upc/prop/clusterxx/Keyboard.java
+++ src/main/java/edu/upc/prop/clusterxx/Keyboard.java
@@ -1,4 +1,45 @@
 package edu.upc.prop.clusterxx;
 
+
+import java.time.Instant;
+
 public class Keyboard {
+    private String nom;
+    private Instant dataCreacio;
+
+    private Instant ultimaModificacio;
+
+    private char[] distribucio;
+
+    private Alphabet Alph;
+
+    private Frequency Freq;
+
+    private Grid Grid;
+
+    public void setNom(String nom) {
+        this.nom = nom;
+    }
+
+    public void setAlph(Alphabet alph) {
+        Alph = alph;
+    }
+
+    public Keyboard(){}
+    public Keyboard(String nom, Alphabet alphabet, Frequency frequency, Grid grid){
+        //S'ha de comprobar que la freq. es d'alfabet?
+
+        this.nom=nom;
+
+        dataCreacio=Instant.now();
+
+        Alph=alphabet;
+
+        Freq = frequency;
+
+        Grid = grid;
+
+    }
+
 }
+
