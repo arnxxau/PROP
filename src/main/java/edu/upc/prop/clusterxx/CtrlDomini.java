38445c732b299d86c25f1b644a1d84a946c01156
Ivian34 <ivancobosnavarro@gmail.com>
2023-11-17 12:08:09 +0100

    He començat el Driver_FP

---
+++ src/main/java/edu/upc/prop/clusterxx/CtrlDomini.java
@@ -0,0 +1,13 @@
+package edu.upc.prop.clusterxx;
+
+import java.util.TreeMap;
+
+public class CtrlDomini {
+
+    TreeMap<String,Alphabet> AP;
+    public CtrlDomini(){}
+
+    public void Afegir_Alfabet(){
+       // AP.new Alphabet();
+    }
+}
