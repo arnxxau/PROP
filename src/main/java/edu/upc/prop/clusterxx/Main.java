384f533f4a108df374140d2447bcab3b7adabb54
arnxxau <arnxxau@gmail.com>
2023-11-18 15:58:18 +0100

    freq class

--- src/main/java/edu/upc/prop/clusterxx/Main.java
+++ src/main/java/edu/upc/prop/clusterxx/Main.java
@@ -3,9 +3,14 @@ package edu.upc.prop.clusterxx;
 import com.google.gson.Gson;
 
 import java.io.FileNotFoundException;
+import java.nio.charset.Charset;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.List;
 
 public class Main {
-  public static void main(String[] args) throws FileNotFoundException {
+  public static void main(String[] args) throws Exception {
     System.out.println("Hello world!");
     //new Gson();
 
@@ -15,10 +20,14 @@ public class Main {
     //Frequency freq = new Frequency("test", "/home/akira/IdeaProjects/subgrup-prop12.5/src/main/java/edu/upc/prop/clusterxx/raw.freq", 0);
     //freq.printFrequencies();
 
+    List<String> lines = Files.readAllLines(Path.of("/home/akira/IdeaProjects/subgrup-prop12.5/src/main/java/edu/upc/prop/clusterxx/text.freq"), StandardCharsets.UTF_8);
+    String[] arr = lines.toArray(new String[0]);
+    Alphabet a = null;
 
-    Frequency freq2 = new Frequency("test", "/Users/arnxxau/FIB/subgrup-prop12.5/src/main/java/edu/upc/prop/clusterxx/text.freq", Frequency.TEXT_MODE);
+
+    Frequency freq2 = new Frequency("test", arr, Frequency.TEXT_MODE, a);
     freq2.printFrequencies();
-    System.out.println(freq2.getFrequency('a', 'c'));
+    System.out.println(freq2.getFrequency('a', 'p'));
   }
 
   public float division(int a, int b) throws ArithmeticException {
