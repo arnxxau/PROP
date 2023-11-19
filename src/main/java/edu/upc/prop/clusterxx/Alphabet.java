3ffc80907e8d93d47c039de2d50701b851530c3c
arnxxau <arnxxau@gmail.com>
2023-11-19 16:38:24 +0100

    javadoc

--- src/main/java/edu/upc/prop/clusterxx/Alphabet.java
+++ src/main/java/edu/upc/prop/clusterxx/Alphabet.java
@@ -1,7 +1,14 @@
+/**
+ * La classe Alphabet representa un alfabet amb un conjunt de caràcters i
+ * freqüències associades a aquest alfabet.
+ *
+ * @author Andreu Bravo
+ * @version entrega 1.0
+ */
+
 package edu.upc.prop.clusterxx;
 
 import java.time.Instant;
-import java.util.ArrayList;
 import java.util.HashSet;
 import java.util.TreeMap;
 
@@ -13,6 +20,13 @@ public class Alphabet {
     private TreeMap<String, Frequency> frequencies;
 
     // Constructor
+
+    /**
+     * Constructor de la classe Alphabet.
+     *
+     * @param nom  El nom de l'alfabet.
+     * @param cars El conjunt de caràcters associat a l'alfabet.
+     */
     public Alphabet(String nom, HashSet<Character> cars) {
         this.nom = nom;
         this.caracters = cars;
@@ -20,10 +34,30 @@ public class Alphabet {
         this.ultMod = Instant.now();
         this.frequencies = new TreeMap<String,Frequency>();
     }
+
+    /**
+     * Obté la grandària de l'alfabet, és a dir, el nombre de caràcters que conté.
+     *
+     * @return La grandària de l'alfabet.
+     */
     public int size() { return caracters.size(); }
+
+    /**
+     * Comprova si un caràcter existeix a l'alfabet.
+     *
+     * @param caracter El caràcter a comprovar.
+     * @return Cert si el caràcter existeix, fals altrament.
+     */
     public boolean existsCharacter(char caracter) {
         return caracters.contains(caracter);
     }
+
+    /**
+     * Afegeix un caràcter a l'alfabet.
+     *
+     * @param caracter El caràcter a afegir.
+     * @return Cert si el caràcter s'ha afegit amb èxit, fals si ja existeix.
+     */
     public boolean addCharacter(char caracter) {
         if (caracters.add(caracter)) {
             ultMod = Instant.now();
@@ -33,6 +67,12 @@ public class Alphabet {
         }
     }
 
+    /**
+     * Elimina un caràcter de l'alfabet.
+     *
+     * @param caracter El caràcter a eliminar.
+     * @return Cert si el caràcter s'ha eliminat amb èxit, fals si no existeix.
+     */
     public boolean deleteCharacter(char caracter) {
         if (caracters.remove(caracter)) {
             ultMod = Instant.now();
@@ -41,14 +81,33 @@ public class Alphabet {
             return false;
         }
     }
+
+    /**
+     * Comprova si una freqüència existeix a l'alfabet.
+     *
+     * @param nomF El nom de la freqüència a comprovar.
+     * @return Cert si la freqüència existeix, fals altrament.
+     */
     public boolean hasFrequency(String nomF) { return frequencies.containsKey(nomF);}
 
+    /**
+     * Afegeix una freqüència a l'alfabet.
+     *
+     * @param frequencia La freqüència a afegir.
+     * @return Cert si la freqüència s'ha afegit amb èxit, fals si ja existeix.
+     */
     public boolean addFrequency(Frequency frequencia) {
         if(frequencies.containsKey(frequencia.getName())) return false;
         frequencies.put(frequencia.getName(), frequencia);
         return true;
     }
 
+    /**
+     * Elimina una freqüència de l'alfabet.
+     *
+     * @param frequencia La freqüència a eliminar.
+     * @return Cert si la freqüència s'ha eliminat amb èxit, fals si no existeix.
+     */
     public boolean deleteFrequency(Frequency frequencia) {
         if (!frequencies.containsKey(frequencia.getName())) return false;
         frequencies.remove(frequencia.getName());
@@ -56,26 +115,57 @@ public class Alphabet {
     }
 
     // Getters
+
+    /**
+     * Obté el nom de l'alfabet.
+     *
+     * @return El nom de l'alfabet.
+     */
     public String getName() {
         return nom;
     }
 
+    /**
+     * Obté la data de creació de l'alfabet.
+     *
+     * @return La data de creació.
+     */
     public Instant getCrDate() {
         return crDate;
     }
 
+    /**
+     * Obté la data de l'última modificació de l'alfabet.
+     *
+     * @return La data de l'última modificació.
+     */
     public Instant getLastMod() {
         return ultMod;
     }
 
+    /**
+     * Obté una còpia del conjunt de caràcters de l'alfabet.
+     *
+     * @return Una còpia del conjunt de caràcters.
+     */
     public HashSet<Character> getCharacters() {
         return new HashSet<>(caracters);
     }
 
+    /**
+     * Obté les freqüències associades a l'alfabet.
+     *
+     * @return Les freqüències associades a l'alfabet.
+     */
     public TreeMap<String, Frequency> getFrequencies() {
         return frequencies;
     }
 
+    /**
+     * Estableix el nom de l'alfabet.
+     *
+     * @param nom El nou nom de l'alfabet.
+     */
     // Setters
     public void setName(String nom) {
         this.nom = nom;
--- src/main/java/edu/upc/prop/clusterxx/Keyboard.java
+++ src/main/java/edu/upc/prop/clusterxx/Keyboard.java
@@ -1,3 +1,11 @@
+/**
+ * La classe Keyboard representa un teclat amb una distribució de lletres en un patró definides per
+ * un alfabet, una freqüència i una graella.
+ *
+ * @author unknown
+ * @version entrega 1.0
+ */
+
 package edu.upc.prop.clusterxx;
 
 import java.time.Instant;
@@ -12,8 +20,16 @@ public class Keyboard {
     private Frequency Freq;
     private Grid Grid;
 
-    //pre: la frequència pertany a l'alfabet
-    //pre: el grid té prous posicions vàlides per posar totes les lletres de l'alfabet
+
+    /**
+     * Constructor de la classe Keyboard.
+     *
+     * @param nom       El nom del teclat.
+     * @param alphabet  L'alfabet associat al teclat.
+     * @param frequency La freqüència associada al teclat.
+     * @param grid      La graella que defineix les posicions vàlides per al teclat.
+     * @throws IllegalArgumentException Si la freqüència no pertany a l'alfabet o si el grid no té prou posicions vàlides.
+     */
     public Keyboard(String nom, Alphabet alphabet, Frequency frequency, Grid grid){
         this.nom = nom;
         this.dataCreacio = Instant.now();
@@ -23,9 +39,19 @@ public class Keyboard {
         this.Grid = grid;
         this.distribucio = new char[]{'a', 'b', 'e'}; // CALCULAR DISTRIBUCIÓ AMB QAP
     }
+
+    /**
+     * Actualitza la distribució de lletres del teclat.
+     */
     public void update() {
         this.distribucio = null; // RECALCULAR DISTRIBUCIÓ
     }
+
+    /**
+     * Obté una representació de la distribució de lletres en el teclat basada en la graella.
+     *
+     * @return Una cadena que representa la distribució de lletres en el teclat.
+     */
     public String getLayout() {
         char[][] mat = new char[Grid.getMaxSize().x][Grid.getMaxSize().y];
         ArrayList<Pair> al = Grid.getPositions();
@@ -46,41 +72,113 @@ public class Keyboard {
     }
 
     // Getters
+
+    /**
+     * Obté el nom del teclat.
+     *
+     * @return El nom del teclat.
+     */
     public String getName() {
         return nom;
     }
+
+    /**
+     * Obté la data de creació del teclat.
+     *
+     * @return La data de creació.
+     */
     public Instant getCrDate() {
         return dataCreacio;
     }
+
+    /**
+     * Obté la data de l'última modificació del teclat.
+     *
+     * @return La data de l'última modificació.
+     */
     public Instant getLastMod() {
         return ultimaModificacio;
     }
+
+    /**
+     * Obté la distribució de lletres actual del teclat.
+     *
+     * @return La distribució de lletres.
+     */
     public char[] getDistribucio() {
         return distribucio;
     }
+
+    /**
+     * Obté l'alfabet associat al teclat.
+     *
+     * @return L'alfabet associat al teclat.
+     */
     public Alphabet getAlphabet() {
         return Alph;
     }
+
+    /**
+     * Obté la freqüència associada al teclat.
+     *
+     * @return La freqüència associada al teclat.
+     */
     public Frequency getFrequency() {
         return Freq;
     }
+
+    /**
+     * Obté la graella associada al teclat.
+     *
+     * @return La graella associada al teclat.
+     */
     public Grid getGrid() {
         return Grid;
     }
 
     // Setters
+
+    /**
+     * Estableix un nou nom per al teclat.
+     *
+     * @param nom El nou nom del teclat.
+     */
     public void setNom(String nom) {
         this.nom = nom;
     }
+
+    /**
+     * Estableix una nova distribució de lletres per al teclat.
+     *
+     * @param distribucio La nova distribució de lletres.
+     */
     public void setDistribucio(char[] distribucio) {
         this.distribucio = distribucio;
     }
+
+    /**
+     * Estableix un nou alfabet per al teclat.
+     *
+     * @param alph El nou alfabet del teclat.
+     */
     public void setAlph(Alphabet alph) {
         this.Alph = alph;
     }
+
+    /**
+     * Estableix una nova freqüència per al teclat.
+     *
+     * @param freq La nova freqüència del teclat.
+     */
     public void setFreq(Frequency freq) {
         this.Freq = freq;
     }
+
+    /**
+     * Estableix una nova graella per al teclat.
+     *
+     * @param grid La nova graella del teclat.
+     */
     public void setGrid(Grid grid) {
         this.Grid = grid;
     }
